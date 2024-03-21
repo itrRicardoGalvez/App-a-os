0335306174d2d6cba17f95963794d379c4a7745d
Ricardo <20230332@ricaldone.edu.sv>
2024-03-21 11:29:47 -0600

    Se creo la app

---
+++ app/src/main/java/ricardo/galvez/myapplication/Calcular.kt
@@ -0,0 +1,11 @@
+package ricardo.galvez.myapplication
+
+class Calcular {
+
+    fun Sumar (Edad: Int): Int{
+        var resultado: Int
+
+        resultado = 2024 - Edad
+        return resultado
+    }
+}
